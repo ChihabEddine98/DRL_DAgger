d6b789c714c44c03cd1fcd55a9f609cd18573eb4
= <ga_benamara@esi.dz>
2022-01-21 18:40:18 +0100

    [HW1] -- DAgger fill on the gaps TODO 1--15

---
+++ cs285/policies/MLP_policy.py
@@ -0,0 +1,125 @@
+import abc
+import itertools
+from typing import Any
+from torch import nn
+from torch.nn import functional as F
+from torch import optim
+
+import numpy as np
+import torch
+from torch import distributions
+
+from cs285.infrastructure import pytorch_util as ptu
+from cs285.policies.base_policy import BasePolicy
+
+
+class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
+
+    def __init__(self,
+                 ac_dim,
+                 ob_dim,
+                 n_layers,
+                 size,
+                 discrete=False,
+                 learning_rate=1e-4,
+                 training=True,
+                 nn_baseline=False,
+                 **kwargs
+                 ):
+        super().__init__(**kwargs)
+
+        # init vars
+        self.ac_dim = ac_dim
+        self.ob_dim = ob_dim
+        self.n_layers = n_layers
+        self.discrete = discrete
+        self.size = size
+        self.learning_rate = learning_rate
+        self.training = training
+        self.nn_baseline = nn_baseline
+
+        if self.discrete:
+            self.logits_na = ptu.build_mlp(
+                input_size=self.ob_dim,
+                output_size=self.ac_dim,
+                n_layers=self.n_layers,
+                size=self.size,
+            )
+            self.logits_na.to(ptu.device)
+            self.mean_net = None
+            self.logstd = None
+            self.optimizer = optim.Adam(self.logits_na.parameters(),
+                                        self.learning_rate)
+        else:
+            self.logits_na = None
+            self.mean_net = ptu.build_mlp(
+                input_size=self.ob_dim,
+                output_size=self.ac_dim,
+                n_layers=self.n_layers, size=self.size,
+            )
+            self.mean_net.to(ptu.device)
+            self.logstd = nn.Parameter(
+                torch.zeros(self.ac_dim, dtype=torch.float32, device=ptu.device)
+            )
+            self.logstd.to(ptu.device)
+            self.optimizer = optim.Adam(
+                itertools.chain([self.logstd], self.mean_net.parameters()),
+                self.learning_rate
+            )
+
+    ##################################
+
+    def save(self, filepath):
+        torch.save(self.state_dict(), filepath)
+
+    ##################################
+
+    def get_action(self, obs: np.ndarray) -> np.ndarray:
+        if len(obs.shape) > 1:
+            observation = obs
+        else:
+            observation = obs[None]
+
+        '''
+           TODO #6 ✅ : return the action that the policy prescribes
+        '''
+        return ptu.to_numpy(self.forward(ptu.from_numpy(observation)))
+
+    # update/train this policy
+    def update(self, observations, actions, **kwargs):
+        raise NotImplementedError
+
+    # This function defines the forward pass of the network.
+    # You can return anything you want, but you should be able to differentiate
+    # through it. For example, you can return a torch.FloatTensor. You can also
+    # return more flexible objects, such as a
+    # `torch.distributions.Distribution` object. It's up to you!
+    '''
+       TODO #7 ✅ : Forward pass for MLPolicy
+    '''
+    def forward(self, observation: torch.FloatTensor) -> Any:
+        if self.discrete:
+           return self.logits_na(observation)
+        return self.mean_net(observation)
+
+
+#####################################################
+#####################################################
+
+class MLPPolicySL(MLPPolicy):
+    def __init__(self, ac_dim, ob_dim, n_layers, size, **kwargs):
+        super().__init__(ac_dim, ob_dim, n_layers, size, **kwargs)
+        self.loss = nn.MSELoss()
+
+    def update(self,observations,actions,adv_n=None,acs_labels_na=None,qvals=None):
+        '''
+           TODO #8 ✅: update the policy and return the loss
+        '''
+        self.optimizer.zero_grad()
+        outputs = self.forward(ptu.from_numpy(observations))
+        loss = self.loss.forward(outputs, ptu.from_numpy(actions))
+        loss.backward()
+        self.optimizer.step()
+        return {
+            'Training Loss': ptu.to_numpy(loss),
+        }
